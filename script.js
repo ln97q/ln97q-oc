95dd5df0b52578a0577319419926ad8bad4cacd0
ln97q <hkd64zzy2h@privaterelay.appleid.com>
2026-07-13 00:49:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const cards=document.querySelectorAll(".card");
 
+cards.forEach(card=>{
+
+card.addEventListener("mousemove",e=>{
+
+const x=e.offsetX/card.clientWidth-.5;
+
+const y=e.offsetY/card.clientHeight-.5;
+
+card.style.transform=
+`rotateY(${x*12}deg) rotateX(${-y*12}deg) translateY(-8px)`;
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform="rotateX(0) rotateY(0)";
+
+});
+
+});
