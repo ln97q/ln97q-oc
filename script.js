54871ed9c76df0eb83b9bdecb7a33f05a861cd25
ln97q <hkd64zzy2h@privaterelay.appleid.com>
2026-07-13 00:54:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const cards=document.querySelectorAll(".card");
+// カードがふわっと表示
+const items = document.querySelectorAll(".character");
 
-cards.forEach(card=>{
-
-card.addEventListener("mousemove",e=>{
-
-const x=e.offsetX/card.clientWidth-.5;
-
-const y=e.offsetY/card.clientHeight-.5;
-
-card.style.transform=
-`rotateY(${x*12}deg) rotateX(${-y*12}deg) translateY(-8px)`;
-
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+  });
+},{
+  threshold:0.2
 });
 
-card.addEventListener("mouseleave",()=>{
-
-card.style.transform="rotateX(0) rotateY(0)";
-
+items.forEach(item=>{
+  observer.observe(item);
 });
+
+// カードを少し傾ける
+items.forEach(card=>{
+
+  card.addEventListener("mousemove",(e)=>{
+
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateY = (x / rect.width - 0.5) * 10;
+    const rotateX = -(y / rect.height - 0.5) * 10;
+
+    card.style.transform =
+      `perspective(900px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-8px)`;
+
+  });
+
+  card.addEventListener("mouseleave",()=>{
+
+    card.style.transform =
+      "perspective(900px) rotateX(0) rotateY(0) translateY(0)";
+
+  });
 
 });
